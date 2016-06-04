3d91df1ab1c11af2573680b6460d588a1adf57c1
Pawel Mstowski <pawel.mstowski@gmail.com>
2016-06-04 23:47:37 +0200

    usuwanie niepotrzebnych dyrektyw using

--- InwersjaTomograficzna.Core/CoreWorker.cs
+++ InwersjaTomograficzna.Core/CoreWorker.cs
@@ -1,11 +1,7 @@
 ﻿using InwersjaTomograficzna.Core.RayDensity.DataReaders.Mocks;
 using InwersjaTomograficzna.Core.RayDensity.DataStructures;
 using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms.DataVisualization.Charting;
 
 namespace InwersjaTomograficzna.Core
