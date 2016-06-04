24aacaeafd038f66448fd6cd18f44a89afa50ce9
Pawel Mstowski <pawel.mstowski@gmail.com>
2016-06-04 10:47:39 +0200

    Dodanie klasy sortujacej punkty i  metod rozszerzajacych do klasy Point

---
+++ InwersjaTomograficzna.Core.TraceRouting/Helpers/PointExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace InwersjaTomograficzna.Core.TraceRouting.Helpers
+{
+    public static class PointExtension
+    {
+        public static double Distance(this Point startPoint, Point endPoint)
+        {
+            return Math.Sqrt(Math.Pow(startPoint.X - endPoint.X, 2) + Math.Pow(startPoint.Y - endPoint.Y, 2));
+        }
+    }
+}
---
+++ InwersjaTomograficzna.Core.TraceRouting/Helpers/PointsSort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace InwersjaTomograficzna.Core.TraceRouting.Helpers
+{
+    public static class PointsSort
+    {
+        public static List<Point> SortByDistanceFromPoint(Point startPoint, List<Point> list)
+        {
+            List<Point> result = new List<Point>();
+            result.Add(startPoint);
+            list.Remove(startPoint);
+            int loopVar = list.Count();
+
+            for(int i=0; i< loopVar; i++)
+            {
+                Point nearestPoint = NearestPointFromList(startPoint, list);
+                result.Add(nearestPoint);
+                list.Remove(nearestPoint);
+            }
+
+            return result;
+        }
+
+        public static Point NearestPointFromList(Point srcPoint, List<Point> listOfPoints)
+        {
+            KeyValuePair<double, int> smallestDistance = new KeyValuePair<double, int>();
+            for(int i=0;i<listOfPoints.Count; i++)
+            {
+                double distance = srcPoint.Distance(listOfPoints[i]);
+                if (i == 0)
+                {
+                    smallestDistance = new KeyValuePair<double, int>(distance, i);
+                }
+                else
+                {
+                    if(distance < smallestDistance.Key)
+                    {
+                        smallestDistance = new KeyValuePair<double, int>(distance, i);
+                    }
+                }
+            }
+            return listOfPoints[smallestDistance.Value];
+        }
+    }
+}
